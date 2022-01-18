86d0f27cdf23e3619843bac23c569cb9c89aa666
mstyatyugin <mastyatyugin@meta-devs.io>
2022-01-18 21:13:48 +0300

    Добавлены пустые строки между логическими блоками, убрал не нужные комментарии

--- src/Lesson_1/Main.java
+++ src/Lesson_1/Main.java
@@ -1,21 +1,19 @@
 package Lesson_1;
-
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-
         int value1 = 0;
         int value2 = 5;
         int sum = value1 + value2;
         System.out.println("Сумма " + sum);
+
         int multiply = value1 * value2;
         System.out.println("Произведение " + multiply);
+
         int difference = value1 - value2;
         System.out.println("Разность " + difference);
-        //Scanner scanner = new Scanner(System.in);
-        String userInput = new Scanner(System.in).next();
+
+              String userInput = new Scanner(System.in).next();
         System.out.println(userInput);
 
 
