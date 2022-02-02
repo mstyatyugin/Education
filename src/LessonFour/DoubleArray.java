6e18b4c847610449a70d2c9449a36645180885b9
mstyatyugin <mastyatyugin@meta-devs.io>
2022-02-02 19:17:28 +0300

    LessonFour

---
+++ src/LessonFour/DoubleArray.java
@@ -0,0 +1,14 @@
+package LessonFour;
+
+
+    public class DoubleArray {
+        public static void main(String[] args) {
+            int[][] array = new int[8][8];
+            array[0][0] = 5;
+            array[0][1] = 55;
+
+            System.out.println(array[0][1] + array[0][0]);
+            System.out.println(array[1][0] + array[0][0]);
+        }
+    }
+
---
+++ src/LessonFour/LessonFour.java
@@ -0,0 +1,26 @@
+package LessonFour;
+
+public class LessonFour {
+
+    public static void main(String[] args) {
+        // 1980-1985 - выводить как это год
+        int year = 1980;
+
+        for (int j = 0; j < 6; j++) {
+            System.out.println("Год " + year++);
+        }
+        int[] araiV1 = new int[5];
+        System.out.println(araiV1[4]);
+
+        araiV1[3] = 67;
+        for (int j = 0; j < araiV1.length; j++) {
+            System.out.println(araiV1[j]);
+        }
+        araiV1 = new int[]{1, 2, 3, 4, 78};
+        System.out.println(araiV1[4]);
+
+        for (int item : araiV1) {
+            System.out.println(item);
+        }
+    }
+}
