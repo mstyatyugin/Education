e8027661bd34b8009f8764f694bfe831e6f6993e
mstyatyugin <mastyatyugin@meta-devs.io>
2022-03-08 20:31:49 +0300

    Названия переменных изменил, sout "Угадай число" вывел за рамки метода, break поставил.

---
+++ src/RandomNumber/Main.java
@@ -0,0 +1,25 @@
+package RandomNumber;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Угадай число!!!");
+
+        int randomNumber = (int) (Math.random() * 100 + 1);
+        for (int i = 0; i < 10; i++) {
+
+            int UserAnswer = new Scanner(System.in).nextInt();
+            if (UserAnswer == randomNumber) {
+                System.out.println("Верно!!!");
+                break;
+            } else if (UserAnswer > randomNumber) {
+                System.out.println("Число больше загаданного");
+            } else {
+                System.out.println("Число меньше загаданного");
+            }
+        }
+    }
+}
+
