e700e35da8c321b69c0ff68004cb8c0ed1b3bba6
mstyatyugin <mastyatyugin@meta-devs.io>
2022-03-18 20:16:08 +0300

    пустую строку 4 удаляю, но она добавляется при фарматировании ctrl+alt+L

--- src/PaladinsExTwo/PaladinsExTwoCase1.java
+++ src/PaladinsExTwo/PaladinsExTwoCase1.java
@@ -3,10 +3,8 @@ package PaladinsExTwo;
 import java.util.Scanner;
 
 public class PaladinsExTwoCase1 {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int value = scan.nextInt();
         if (value > 10) {
             System.out.println("Число больше 10");
